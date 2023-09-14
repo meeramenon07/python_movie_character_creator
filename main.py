669283d67fee4034d734d0639b5a9eda636534aa
Meera Menon <46912602+meeramenon07@users.noreply.github.com>
2023-09-14 17:56:12 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+print("Mahabharatha Movie Character Creator ")
+print("Which character are you in the Mahabharatha movie \n\n")
+truthFacts = input("Do you always tell the truth? : ")
+if truthFacts == "No" :
+  print("Then you're Krishna")
+else:
+  print("Then you're Yudhishthira")
+enemy = input("Who is your deadly enemy?: ")
+if enemy == "None":
+  print("Then you're Yudhishthira")
+else:
+  print("Hey, you're not Yudhishtira")
+
+myBody = input("Are you physically very strong? : ")
+if myBody == "No":
+  print("Then, you're not Bhima")
+else:
+  print("Wow, then, you're the great Bhima !")
+leadership = input("Are you a leader or a follower: ")
+if leadership == "leader":
+  print("Great! You're Krishna, the main leader behind the Pandavas in the Mahabharatha!")
+else:
+  print("You're some other character from Mahabharatha")
+
